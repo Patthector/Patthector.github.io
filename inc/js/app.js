4f3fb8b2ff09b1c913e9f41c350ee6101ff2b006
Patthector <pa77or@gmial.com>
2019-02-22 22:42:58 -0500

    Adding fade out styles to header

--- inc/js/app.js
+++ inc/js/app.js
@@ -6,28 +6,77 @@ $(document).ready(function(){
     // 2- CHANGE the transparency of the element to white after the <nav> cross
       // the 100vh area
     // 3-when the <nav? element is back to the top; make it appears again
+
   var distance = $("#intro").outerHeight();
-  if ( $(window).scrollTop() >=  distance ){
-    console.log("we should show the navbar");
-    showNavbar();
-  } else{
-    console.log("navbar transparent");
-    hideNavbar();
-  }
 
   $(document).scroll(function(){
-    if ( $(window).scrollTop() >=  distance ){
+    if ( $(window).scrollTop() >=  (distance/4) ){//FULL NAME BEHAVIOR
+      console.log("Hide PATRICIA HECTOR");
+      hideFullName();
+    }
+    if ( $(window).scrollTop() >=  (distance/3) ){//TEXT ACTION BEHAVIOR
+      console.log("Hide action text and button");
+      hideActionText();
+    }
+    if ( $(window).scrollTop() >=  (distance/2) ){//TEXT ACTION BEHAVIOR
+      console.log("Hide action text and button");
+      hideActionButton();
+    }
+    if ( $(window).scrollTop() >=  distance ){//NAVBAR BEHAVIOR
       console.log("we should show the navbar");
       showNavbar();
-    } else{
+    }
+    if( $(window).scrollTop() <  distance ){//NAVBAR BEHAVIOR
       console.log("navbar transparent");
       hideNavbar();
+    }
+    if ( $(window).scrollTop() <  (distance/2) ){//TEXT ACTION BEHAVIOR
+      console.log("Hide action text and button");
+      showActionButton();
+    }
+    if( $(window).scrollTop() <  (distance/3) ){//TEXT ACTION BEHAVIOR
+      console.log("Show action text");
+      showActionText();
+    }
+    if( $(window).scrollTop() <  (distance/4) ){//FULL NAME BEHAVIOR
+      console.log("Show action text");
+      showFullName();
     }
   });
 
   $("#btn-navbar").on( "click", function(){
     showNavbar();
   });
+
+  $(document).on("click", function(e){
+    if( $("#portfolioNav").hasClass("show") ){
+      $("#portfolioNav").removeClass("show");
+    }
+  });
+
+  function hideFullName(){
+    $("h1.cover-heading").css( "opacity", 0 );
+  }
+
+  function showFullName(){
+    $("h1.cover-heading").css( "opacity", 1 );
+  }
+
+  function hideActionText(){
+    $("#intro-text").css( "opacity", 0 );
+  }
+
+  function hideActionButton(){
+    $("#intro-button").css( "opacity", 0 );
+  }
+
+  function showActionText(){
+    $("#intro-text").css( "opacity", 1 );
+  }
+
+  function showActionButton(){
+    $("#intro-button").css( "opacity", 1 );
+  }
 
   function showNavbar(){
     if( !( $("#navbar").css("background-color") === "white" ) ){
